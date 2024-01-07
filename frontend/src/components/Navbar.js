31ad3379c4d57cc8f75162d397a62b1de8a6407f
sanket pote <sanketsanjaypote@gmail.com>
2024-01-07 21:02:25 +0530

    initital commit

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,26 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import '../assets/css/Navbar.css'
 const NavbarSidebar = () => {
+  useEffect(() => {
+    // Include Bootstrap and jQuery script dynamically
+    const script1 = document.createElement('script');
+    script1.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
+    script1.crossOrigin = 'anonymous';
+    script1.integrity = 'sha256-GLT2LmPk1FgPdG7sEC9C1b8tBWTafkdI1j03JmC4z5I=';
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.src = 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js';
+    script2.crossOrigin = 'anonymous';
+    script2.integrity = 'sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCvnbB9PLc+ssAgcfUKFJFj/DAiSNa';
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
   return (
     <div>
       <div className="navbar navbar-inverse navbar-fixed-top">
@@ -16,8 +36,8 @@ const NavbarSidebar = () => {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="../index.php">
-              <p style={{ fontSize: '30px' }}>VIT Connect</p>
+            <a className="navbar-brand" href="/Dash">
+              <p style={{ fontSize: '30px', color:'black' }}>VIT Connect</p>
             </a>
           </div>
         </div>
@@ -26,7 +46,7 @@ const NavbarSidebar = () => {
         <div className="sidebar-collapse">
           <ul className="nav" id="main-menu">
             <li className="active-link">
-              <a href="/dash">
+              <a href="/Dash">
                 <i className="fa fa-desktop "></i>Home
               </a>
             </li>
@@ -37,21 +57,21 @@ const NavbarSidebar = () => {
             </li>
             <li>
               <a href="/study_option">
-                <i className="fa fa-book "></i>Study
+                <i className="fa fa-book "></i>Event Management
               </a>
             </li>
             <li>
               <a href="/buy_sell">
-                <i className="fa fa-lightbulb-o "></i>Buy/Sell
+                <i className="fa fa-lightbulb-o"></i>Trade Old Equipment
               </a>
             </li>
             <li>
               <a href="/roommate">
-                <i className="fa fa-users"></i>Room Mate
+                <i className="fa fa-users"></i>Find Flatmate
               </a>
             </li>
             <li>
-              <a href="/index">
+              <a href="/forumindex">
                 <i className="fa fa-quora" aria-hidden="true" alt="forum image"></i>Community Forum
               </a>
             </li>
@@ -60,6 +80,7 @@ const NavbarSidebar = () => {
                 <i className="fa fa-edit "></i>Logout
               </a>
             </li>
+            {/* Add other list items as needed */}
           </ul>
         </div>
       </nav>
--- frontend/src/pages/wantbike.js
+++ frontend/src/pages/wantbike.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react';
-import NavbarSidebar from '../components/Navbar';
+import React, { useState } from "react";
+import NavbarSidebar from "../components/Navbar";
 
 const Wantbike = () => {
-  const [phoneNo, setPhoneNo] = useState('');
-  const [location, setLocation] = useState('Chintamaninagar2'); // Default location
-  const [department, setDepartment] = useState('AIDS'); // Default department
-  const [year, setYear] = useState('4th'); // Default year
-  const [gender, setGender] = useState('');
+  const [phoneNo, setPhoneNo] = useState("");
+  const [location, setLocation] = useState("Chintamaninagar2"); // Default location
+  const [department, setDepartment] = useState("AIDS"); // Default department
+  const [year, setYear] = useState("4th"); // Default year
+  const [gender, setGender] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // Handle form submission logic here
-    console.log('Form submitted:', { phoneNo, location, department, year, gender });
+    console.log("Form submitted:", {
+      phoneNo,
+      location,
+      department,
+      year,
+      gender,
+    });
   };
 
   return (
     <div>
       <div id="wrapper">
-        <NavbarSidebar/>
+        <NavbarSidebar />
         <div id="page-wrapper">
           <div id="page-inner">
             <div className="content">
@@ -39,9 +45,38 @@ const Wantbike = () => {
                       name="location"
                       className="box"
                       value={location}
-                      onChange={(e) => setLocation(e.target.value)}
-                    >
-                      {/* Add location options */}
+                      onChange={(e) => setLocation(e.target.value)}>
+                      <option name="Bibwewadi" value="Bibwewadi">
+                        Bibwewadi
+                      </option>
+                      <option name="kondhwa" value="kondhwa">
+                        Kondhwa
+                      </option>
+                      <option
+                        name="Lower_Indiranagar"
+                        value="Lower_Indiranagar">
+                        Lower Indiranagar
+                      </option>
+                      <option
+                        name="Upper_Indiranagar"
+                        value="Upper_Indiranagar">
+                        Upper Indiranagar
+                      </option>
+                      <option name="Chintamaninagar1" value="Chintamaninagar1">
+                        Chintamaninagar 1
+                      </option>
+                      <option
+                        name="Chintamaninagar2"
+                        value="Chintamaninagar2"
+                        selected>
+                        Chintamaninagar 2
+                      </option>
+                      <option value="Dhankavdi" name="Dhankavdi">
+                        Dhankavdi
+                      </option>
+                      <option value="Kothrud" name="Kothrud">
+                        Kothrud
+                      </option>
                     </select>
                   </div>
                   <div className="input-box">
@@ -50,9 +85,15 @@ const Wantbike = () => {
                       name="department"
                       className="box"
                       value={department}
-                      onChange={(e) => setDepartment(e.target.value)}
-                    >
-                      {/* Add department options */}
+                      onChange={(e) => setDepartment(e.target.value)}>
+                      <option value="CS">CS</option>
+                      <option value="IT">IT</option>
+                      <option value="AIDS" selected>
+                        AIDS
+                      </option>
+                      <option value="Mechanical">Mechanical</option>
+                      <option value="Chemical">Chemical</option>
+                      <option value="Intrument">Intrument</option>
                     </select>
                   </div>
                   <div className="input-box">
@@ -61,17 +102,43 @@ const Wantbike = () => {
                       name="language"
                       className="box"
                       value={year}
-                      onChange={(e) => setYear(e.target.value)}
-                    >
-                      {/* Add year options */}
+                      onChange={(e) => setYear(e.target.value)}>
+                      <option value="1st">1st</option>
+                      <option value="2nd">2nd</option>
+                      <option value="3rd">3rd</option>
+                      <option value="4th" selected>
+                        4th
+                      </option>
                     </select>
                   </div>
                 </div>
                 <div className="gender-details">
                   {/* Add radio buttons and labels for gender */}
+                  <input type="radio" name="gender" id="dot-1" />
+                  <input type="radio" name="gender" id="dot-2" />
+                  <input type="radio" name="gender" id="dot-3" />
+                  <span>Gender</span>
+                  <div class="category">
+                    <label for="dot-1">
+                      <span class="dot one"></span>
+                      <span class="gender">Male</span>
+                    </label>
+                    <label for="dot-2">
+                      <span class="dot two"></span>
+                      <span class="gender">Female</span>
+                    </label>
+                    <label for="dot-3">
+                      <span class="dot three"></span>
+                      <span class="gender">Prefer not to say</span>
+                    </label>
+                  </div>
                 </div>
                 <div className="button">
-                  <input type="submit" name="submit" value="Get your Bike Partner" />
+                  <input
+                    type="submit"
+                    name="submit"
+                    value="Get your Bike Partner"
+                  />
                 </div>
               </form>
             </div>
